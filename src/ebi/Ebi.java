81823661a15b1e0b5a1185f3e3811518f9e29f26
Paul Morganthall <paul@morganthall.com>
2020-02-10 17:02:04 -0500

    Get ID to associate with all books found.

--- src/ebi/Ebi.java
+++ src/ebi/Ebi.java
@@ -1,7 +1,11 @@
 package ebi;
 
+import javax.swing.JOptionPane;
+
 public class Ebi {
 
 	public static void main(String[] args) {
+		String chestID = JOptionPane.showInputDialog("Enter chest ID to associate with this collection of books:");
+		System.out.println(chestID);
 	}
 }
