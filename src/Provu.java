b616e3dd19401b1df4a6a2a7aecbb9964c8629d9
Paul Morganthall <paul@morganthall.com>
2020-02-24 19:05:17 -0500

    Playground for java trials.

---
+++ src/Provu.java
@@ -0,0 +1,18 @@
+public class Provu {
+
+	public static void main(String[] args) {
+
+		for (int rx : range_0_6()) {
+			System.out.println(rx);
+		}
+	}
+
+	private static int[] range_0_6() {
+		return new int[] {0, 1, 2, 3, 4, 5, 6};
+	}
+
+
+}
+
+
+
