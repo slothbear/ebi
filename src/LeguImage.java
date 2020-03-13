314d5020c4bdb2cd3b6193a115788834efb162c8
Paul Morganthall <paul@morganthall.com>
2020-03-13 11:59:16 -0400

    Format statements
    Eclipse code formatting (first time)
    From preferences, Java, Save actions:
    
    Add final modifier to private fields
    Add final modifier to local variables
    Remove unused imports
    Add missing '@Override' annotations
    Add missing '@Override' annotations to implementations of interface methods
    Add missing '@Deprecated' annotations
    Remove unnecessary casts
    Remove trailing white spaces on all lines
    Correct indentation

--- src/LeguImage.java
+++ src/LeguImage.java
@@ -12,34 +12,37 @@ public class LeguImage {
 	private static final int MAX_ENCHANTMENTS = 7;
 	private static final int CHUNK_WIDTH = 425;
 	private static final int CHUNK_HEIGHT = 35;
-	private static final int BLACK_COLOR = new Color(0,0,0).getRGB();
-	private static final int TEXT_COLOR = new Color(154,154,154).getRGB();
+	private static final int BLACK_COLOR = new Color(0, 0, 0).getRGB();
+	private static final int TEXT_COLOR = new Color(154, 154, 154).getRGB();
 
 	public static void main(String[] args) throws IOException {
-		String fileName = System.getProperty("user.dir") +
+		final String fileName = System.getProperty("user.dir") +
 				"/captures/" + CHEST_SLOT + ".png";
-		BufferedImage popup = ImageIO.read(new File(fileName));
+		final BufferedImage popup = ImageIO.read(new File(fileName));
 		JOptionPane.showMessageDialog(null,
 				"full enchanted book list", "Ebi",
 				JOptionPane.INFORMATION_MESSAGE,
 				new ImageIcon(popup));
 
 		for (int chunkLine = 0; chunkLine < MAX_ENCHANTMENTS; chunkLine++) {
-			BufferedImage chunk = popup.getSubimage(0, CHUNK_HEIGHT * chunkLine, CHUNK_WIDTH, CHUNK_HEIGHT);
-			int[] pixels = chunk.getRGB(0, 0, CHUNK_WIDTH, CHUNK_HEIGHT, null, 0, CHUNK_WIDTH);
-			int count = textPixelCount(pixels);
+			final BufferedImage chunk = popup.getSubimage(0,
+					CHUNK_HEIGHT * chunkLine, CHUNK_WIDTH, CHUNK_HEIGHT);
+			final int[] pixels = chunk.getRGB(0, 0, CHUNK_WIDTH, CHUNK_HEIGHT,
+					null, 0, CHUNK_WIDTH);
+			final int count = textPixelCount(pixels);
 			if (count == 0) {
 				continue; // next chunk
 			}
 
-			int column = chunk.getWidth()-1;
+			int column = chunk.getWidth() - 1;
 			while (column >= 0 && allBlack(chunk, column)) {
 				column--;
-				}
+			}
 
 			JOptionPane.showMessageDialog(null,
-					"enchantment #" + (chunkLine+1) +
-						"\ntext pixels: " + count + "\nfirst text: " + column,
+					"enchantment #" + (chunkLine + 1) +
+							"\ntext pixels: " + count + "\nfirst text: "
+							+ column,
 					"Ebi",
 					JOptionPane.INFORMATION_MESSAGE,
 					new ImageIcon(chunk));
@@ -47,17 +50,17 @@ public class LeguImage {
 	}
 
 	private static boolean allBlack(BufferedImage image, int x) {
-		int[] pixels = image.getRGB(x, 0, 1, CHUNK_HEIGHT, null, 0, 1);
-		for (int y = 0; y<CHUNK_HEIGHT-1; y++) {
+		final int[] pixels = image.getRGB(x, 0, 1, CHUNK_HEIGHT, null, 0, 1);
+		for (int y = 0; y < CHUNK_HEIGHT - 1; y++) {
 			if (pixels[y] != BLACK_COLOR)
-			return false;
+				return false;
 		}
 		return true;
 	}
 
 	private static int textPixelCount(int[] pixels) {
 		int count = 0;
-		for (int pixel : pixels) {
+		for (final int pixel : pixels) {
 			count += (pixel == TEXT_COLOR) ? 1 : 0;
 		}
 		return count;
