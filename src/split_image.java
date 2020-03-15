434528a1212e9439219ebd112eaca9a5477325bb
Paul Morganthall <paul@morganthall.com>
2020-03-15 14:51:31 -0400

    Implement method: strip.
    Code copied from leguImage, including front and back whitespace detection,
    allBlack(), showImage() for debugging.

--- src/split_image.java
+++ src/split_image.java
@@ -1,35 +1,41 @@
+import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
 
 import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
 
 public class split_image {
+	private static final int BLACK_COLOR = new Color(0, 0, 0).getRGB();
 
 	public static void main(String[] args) throws IOException {
+
 		String fileName = System.getProperty("user.dir") +
 			"/captures/single_chunk.png";
-		BufferedImage chunk = ImageIO.read(new File(fileName));
+		BufferedImage image = ImageIO.read(new File(fileName));
+
+		BufferedImage chunk = strip(image);
+		showImage(chunk, "stripped");
+		BufferedImage[] pieces = split(chunk);
+
 		BufferedImage name = null;
 		BufferedImage level = null;
 
-		chunk = strip(chunk);
-		BufferedImage[] pieces = split(chunk);
-
-		BufferedImage lastPiece = pieces[pieces.length - 1];
-		if (isLevel(lastPiece)) {
-			level = lastPiece;
-			BufferedImage[] allButLast = Arrays.copyOfRange(pieces, 0,
-				pieces.length - 1);
-			name = (BufferedImage) join(allButLast);
-		} else {
-			level = null;
-			name = (BufferedImage) join(pieces);
-		}
-
-		System.out.println(name);
-		System.out.println(level);
+//		BufferedImage lastPiece = pieces[pieces.length - 1];
+//		if (isLevel(lastPiece)) {
+//			level = lastPiece;
+//			BufferedImage[] allButLast = Arrays.copyOfRange(pieces, 0,
+//				pieces.length - 1);
+//			name = (BufferedImage) join(allButLast);
+//		} else {
+//			level = null;
+//			name = (BufferedImage) join(pieces);
+//		}
+//
+//		System.out.println(name);
+//		System.out.println(level);
 
 	}
 
@@ -49,8 +55,37 @@ public class split_image {
 	}
 
 	private static BufferedImage strip(BufferedImage chunk) {
+		int column = 0;
+		while (column <= chunk.getWidth() - 1 && allBlack(chunk, column)) {
+			column++;
+		}
+		int firstTextColumn = column;
 
-		return null;
+		column = chunk.getWidth() - 1;
+		while (column >= 0 && allBlack(chunk, column)) {
+			column--;
+		}
+		int lastTextColumn = column;
+
+		return chunk.getSubimage(firstTextColumn, 0,
+			lastTextColumn - firstTextColumn + 1, chunk.getHeight());
+	}
+
+	private static boolean allBlack(BufferedImage image, int x) {
+		int height = image.getHeight();
+		int[] pixels = image.getRGB(x, 0, 1, height, null, 0, 1);
+		for (int y = 0; y < height; y++) {
+			if (pixels[y] != BLACK_COLOR) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	private static void showImage(BufferedImage popup, String message) {
+		JOptionPane.showMessageDialog(null, message,
+			"Ebi", JOptionPane.INFORMATION_MESSAGE,
+			new ImageIcon(popup));
 	}
 
 }
