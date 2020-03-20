970932d25edcf90660d16c5a24a7b332ac0d2746
Paul Morganthall <paul@morganthall.com>
2020-03-20 13:12:30 -0400

    Refactor Ruby example to match method in split_image.
    There was no need to join the “name” pieces, since we only care
    about the number of text pixels in the name. No need to concatenate
    the images. If a display of the name is needed, use the whole chunk
    including the level.

--- pick_apart.rb
+++ pick_apart.rb
@@ -3,12 +3,12 @@
 
 enchantment = "    Depth  Strider  III"
 chunk = whole_line.strip                     # "Depth  Strider  III"
-pieces = chunk.split(WORD_SPACE).            # ["Depth", "Strider", "III"]
+pieces = chunk.split(WORD_SPACE)             # ["Depth", "Strider", "III"]
 
 if %w[I II III IV V].include? pieces.last
   level = pieces.pop                         # "III"
-  name = pieces.join(" ")                    # "Depth Strider"
 else
-  level = ""
-  name = pieces.join(" ")
+  level = 0
 end
+
+namePixels = textPixels(pieces)    # 1072 pixels in "Depth Strider"
