b0d22efbd86b5dde139a2e84f2673c25297a5fdb
Paul Morganthall <paul@morganthall.com>
2020-03-20 13:19:53 -0400

    Extract method for chestID dialog.
    isEmpty() is a nicer interface for missing value than checking
    null and empty in the main() routine.

--- src/Ebi.java
+++ src/Ebi.java
@@ -3,15 +3,23 @@ import javax.swing.JOptionPane;
 public class Ebi {
 
 	public static void main(String[] args) {
-		String chestID = JOptionPane.showInputDialog(null,
+		String chestID = getChestID();
+		if (chestID.isEmpty()) {
+			return;
+		}
+	}
+
+	private static String getChestID() {
+		String id = JOptionPane.showInputDialog(null,
 			"Enter the chest ID to associate with these enchantments.\n" +
 				"You have 10 seconds after pressing OK to open your " +
 				"Large Chest in full screen.",
 			"Ebi",
 			JOptionPane.INFORMATION_MESSAGE);
-		if (chestID == null || chestID.isEmpty()) {
-			return;
+		if (id == null) {
+			id = "";
 		}
+		return id;
 	}
 
 }
