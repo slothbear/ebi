81056e9215c4a4f3be2dd592cd2e0063a853f6fc
Paul Morganthall <paul@morganthall.com>
2020-03-20 11:22:40 -0400

    Add constants for different servers.
    Different servers on different versions with different font colors.

--- src/Ebi.java
+++ src/Ebi.java
@@ -91,7 +91,9 @@ public class Ebi {
 
 
 class EnchantmentColorFilter extends RGBImageFilter {
-	private static final int GRAY_LEVEL = 151;
+	private static final int PICKAXE_GRAY_LEVEL = 151; // Minecraft 15.2
+	private static final int AXEPICK_GRAY_LEVEL = 154; // Minecraft 13.2
+	private static final int GRAY_LEVEL = AXEPICK_GRAY_LEVEL;
 	private static int blackColor = Color.black.getRGB();
 	private static int enchTextColor = new Color(
 		GRAY_LEVEL, GRAY_LEVEL, GRAY_LEVEL).getRGB();
