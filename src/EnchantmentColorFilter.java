f4da1f20be0a7f5f27f5d47b80372f5d99ebed02
Paul Morganthall <paul@morganthall.com>
2020-03-26 13:43:01 -0400

    Move to Pickaxe color setting.
    Axepick is dead except for testing, so leave the constant for now.

--- src/EnchantmentColorFilter.java
+++ src/EnchantmentColorFilter.java
@@ -5,7 +5,7 @@ public class EnchantmentColorFilter extends RGBImageFilter {
 	@SuppressWarnings("unused")
 	private static final int PICKAXE_GRAY_LEVEL = 151; // Minecraft 15.2
 	private static final int AXEPICK_GRAY_LEVEL = 154; // Minecraft 13.2
-	private static final int GRAY_LEVEL = AXEPICK_GRAY_LEVEL;
+	private static final int GRAY_LEVEL = PICKAXE_GRAY_LEVEL;
 	private static int blackColor = Color.black.getRGB();
 	private static int enchTextColor = new Color(
 		GRAY_LEVEL, GRAY_LEVEL, GRAY_LEVEL).getRGB();
