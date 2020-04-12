8a55eced186afa765bf31e148d9ef28098859461
Paul Morganthall <paul@morganthall.com>
2020-04-12 15:12:07 -0400

    Replace pixelMap with spellVault.
    The number of pixels is now stored in Spells, a special case of
    Enchantments.
    
    Use a multi-step initialization process to make it easier to copy/paste
    a new list of spells from a previous run’s output.
    
    Remove the properties support since everything is now driven by 
    Spells in the code.
    
    (jOptionPane just moved, no changes.)

--- src/Distiller.java
+++ src/Distiller.java
@@ -1,12 +1,9 @@
 import java.awt.Color;
 import java.awt.Dimension;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
-import java.util.Properties;
 
 import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
@@ -14,9 +11,28 @@ import javax.swing.UIManager;
 import javax.swing.plaf.ColorUIResource;
 
 public class Distiller {
-	static Map<Integer, String> pixelMap = new HashMap<Integer, String>();
+	static List<Enchantment> spells = new ArrayList<Enchantment>();
 	private static List<String> options = new ArrayList<String>();
+
+	private static Enchantment[] spellVault = new Enchantment[] {
+		// @formatter:off (so each enchantment stays on one line)
+		new Enchantment("Punch", 0, "", 0, 0, null, 101, 101, 636, 648),
+		new Enchantment("Mending", 0, "", 0, 0, null, 129, 129, 969, 969),
+		new Enchantment("Respiration", 0, "", 0, 0, null, 192, 192, 1293, 1293),
+		new Enchantment("Punch", 0, "", 0, 0, null, 101, 101, 636, 648),
+		new Enchantment("Loyalty", 0, "", 0, 0, null, 125, 126, 843, 846),
+		new Enchantment("Channeling", 0, "", 0, 0, null, 181, 181, 1248, 1248),
+		new Enchantment("Flame", 0, "", 0, 0, null, 90, 90, 624, 624),
+		new Enchantment("Power", 0, "", 0, 0, null, 101, 101, 654, 654),
+		new Enchantment("Sharpness", 0, "", 0, 0, null, 185, 185, 1200, 1200),
+		new Enchantment("Loyalty", 0, "", 0, 0, null, 125, 126, 843, 846)
+		// @formatter:on
+	};
+	private static List<Enchantment> spellChest = Arrays.asList(spellVault);
+
 	static {
+		spells.addAll(spellChest);
+
 		options.add("Aqua Afifinity");
 		options.add("Bane of Arthropods");
 		options.add("Blast Protection");
@@ -61,47 +77,29 @@ public class Distiller {
 	public static void distill(List<Enchantment> enchantments)
 		throws IOException {
 		setDialogOptions();
-		loadProperties();
 
 		for (Enchantment enchantment : enchantments) {
-			if (!enchantment.name.isEmpty()) {
-				continue;
-			}
 
-			if (pixelMap.containsKey(enchantment.namePixels)) {
-				enchantment.name = pixelMap.get(enchantment.namePixels);
-			} else {
-				String choice = (String) JOptionPane.showInputDialog(null,
-					"Which enchantment is this?\n",
-					"Ebi",
-					JOptionPane.QUESTION_MESSAGE,
-					new ImageIcon(enchantment.image),
-					options.toArray(),
-					options.get(0));
 
-				if (choice == null || choice.isEmpty()) {
-					JOptionPane.showMessageDialog(null,
-						"Quitting: CANCEL or nothing selected");
-					System.exit(12);
-				}
-
-				enchantment.name = choice;
-				pixelMap.put(enchantment.namePixels, choice);
 			}
 		}
 	}
 
-	private static void loadProperties() throws IOException {
-		FileInputStream in = new FileInputStream("ebi.properties");
-		Properties props = new Properties();
-		props.load(in);
-		in.close();
+	private static String promptForName(Enchantment enchantment) {
+		String choice = (String) JOptionPane.showInputDialog(null,
+			"Which enchantment is this?\n",
+			"Ebi",
+			JOptionPane.QUESTION_MESSAGE,
+			new ImageIcon(enchantment.image),
+			options.toArray(),
+			options.get(0));
 
-		for (Object keyO : props.keySet()) {
-			String key = (String) keyO;
-			String value = props.getProperty(key);
-			pixelMap.put(Integer.parseInt(key), value);
+		if (choice == null || choice.isEmpty()) {
+			JOptionPane.showMessageDialog(null,
+				"Quitting: CANCEL or nothing selected");
+			System.exit(12);
 		}
+		return choice;
 	}
 
 	private static void setDialogOptions() {
